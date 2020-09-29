28b815c3f4f3b248fde016e22b2edeea4b92d7a2
Senja Jarva <senja.jarva@futurice.com>
2020-09-29 20:02:17 +0300

    Updated stuff

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,17 +1,10 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { Button } from "./Button";
-
-/*
-storiesOf("Button", module).add("Default button", () => (
-    <Button label="TESTING"></Button>
-));*/
+import { Button } from './Button';
 
 export default {
-    title: "Button",
+    title: 'Button',
     component: Button,
 };
 
-export const DefaultWithoutLabel = () => <Button label="TESTING"></Button>;
+export const DefaultWithoutLabel = () => <Button label='TESTING'></Button>;
